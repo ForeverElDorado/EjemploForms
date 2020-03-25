8ff76c13c7ed759afdb272d20ddead4032d3b982
aghsk <aghski@gmail.com>
2020-03-25 11:23:38 +0100

    Agregar archivos de proyecto.

---
+++ EjemploForms/VentanaPrincipal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploForms
+{
+    public partial class VentanaPrincipal : Form
+    {
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+        }
+        //Metodo para cerrar la app entera cuando se cerra el form
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.WindowsShutDown) return;
+            Application.Exit();
+        }
+
+        private void botonCalendario(object sender, EventArgs e)
+        {
+            Ventana01 ventana = new Ventana01();
+            ventana.Show();
+        }
+
+        private void botonImagen(object sender, EventArgs e)
+        {
+            Ventana02 ventana = new Ventana02();
+            ventana.Show();
+        }
+    }
+}
